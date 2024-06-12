3688a1fb879eda72fdc82b4bb13866a370031cd0
jsum01 <djadjawltjr@gmail.com>
2024-06-12 11:29:40 +0900

    Temp

--- app/src/main/java/kr/ac/uc/beverage_ordering_system/MainActivity.java
+++ app/src/main/java/kr/ac/uc/beverage_ordering_system/MainActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.uc.beverage_ordering_system;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +12,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btAddOrder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-
+        btAddOrder = findViewById(R.id.btAddOrder);
+        btAddOrder.setOnClickListener(v -> goOrder());
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void goOrder() {
+        Intent intent = new Intent(MainActivity.this, OnE_Activity.this);
     }
 }
