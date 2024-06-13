b69fc470ee824459ea8c9e3b1bade273a9ac3585
YongJoo <slfkalstks@naver.com>
2024-06-13 16:20:16 +0900

    Merge_MainActivity
    Merge

--- app/src/main/java/kr/ac/uc/beverage_ordering_system/MainActivity.java
+++ app/src/main/java/kr/ac/uc/beverage_ordering_system/MainActivity.java
@@ -22,12 +22,8 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-<<<<<<< Updated upstream
         btAddOrder = findViewById(R.id.btAddOrder);
         btAddOrder.setOnClickListener(v -> goOrder());
-=======
-
->>>>>>> Stashed changes
 
         tvCustomerName = findViewById(R.id.tvCustomerName);
         tvBeverageName = findViewById(R.id.tvBeverageName);
@@ -43,12 +39,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void goOrder() {
-<<<<<<< HEAD
-        Intent intent = new Intent(MainActivity.this, OnE_Activity.class);
-=======
         Intent intent = new Intent(this, OnE_Activity.class);
         startActivity(intent);
         finish();
->>>>>>> 1108bceac19a35443cd967e39964b9b31f182175
     }
 }
